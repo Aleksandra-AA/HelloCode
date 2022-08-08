1cb33d89f82b2972279e5f12736a6d48d6e95a23
AAA <shurozka333@mail.com>
2022-08-08 22:30:53 +0300

    Example010 Metod Array

---
+++ Example010_MetodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = {1, 2, 4, 68, 15, 3, 99, 15, 68};
+
+int n = array.Length;
+int find = 68;
+
+int index = 0;
+
+while(index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++; 
+}
